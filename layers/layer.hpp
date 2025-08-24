252a25b5186d71ab124746fb0139fe89191fe864
tanmoyyysarkar <tanmoysarkar.ts1234@gmail.com>
2025-08-24 18:59:23 +0530

    created the base layer class

---
+++ layers/layer.hpp
@@ -0,0 +1,13 @@
+// creating a base layer so that all layers have the same structure
+
+#pragma once
+#include <vector>
+
+using namespace std;
+
+class layer
+{
+    virtual vector<vector<vector<float>>> forward(const vector<vector<vector<float>>> &input) = 0;
+
+    virtual ~layer() = default;
+};
